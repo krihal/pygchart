64712f773f584bf8d18596047a3f8290de18a6d2
Kristofer Hallin <khallin@accedian.com>
2012-12-27 22:55:19 +0100

    Read files from data/ directory, and other changes

--- pygchart.py
+++ pygchart.py
@@ -1,62 +1,43 @@
 import os
-import csv
-import sys
-import getopt
 
 from pygooglechart import Chart
 from pygooglechart import SimpleLineChart
 from pygooglechart import Axis
 
-def open_csv(file):
-    labels = []
-    values = []
+def dataparser(filename):
+    dates = [0]
+    funds = [0]
+    values = [0]
 
-    with open(file, 'rb') as csvfile:
-        cr = csv.reader(csvfile, delimiter=',')
-        for row in cr:
-            labels.append(row[0])
-            values.append(int(row[1]))
+    fd = open(filename, "r")
+    
+    for line in fd:
+        (date, fund, value) = line.split(";")
+        dates.append(date)
+        funds.append(fund)
+        values.append(int(float(value.strip())))
 
-    csvfile.close()
+    fd.close()
 
-    return labels, values
+    return (dates, values)
+        
+def plotter(fund):
+    
+    (dates, values) = dataparser("data/%s" % fund)
 
-def plot(labels, values, filename):
-    filename, fileext = os.path.splitext(filename)
-
-    chart = SimpleLineChart(600, 375, y_range=[min(values), max(values)])
+    left_axis = [int(min(values)), 0, int(max(values) + 1)]
+    left_axis[0] = 0
+    
+    chart = SimpleLineChart(600, 375, y_range=[min(values), max(values) + 1])
     chart.add_data(values)
     chart.set_colours(['0000FF'])
     chart.set_grid(0, 25, 5, 5)
-
-    left_axis = [int(min(values) - 0.5), 0, int(max(values) + 0.5)]
-    left_axis[0] = ''
-
     chart.set_axis_labels(Axis.LEFT, left_axis)
-    chart.set_axis_labels(Axis.BOTTOM, labels)
-    chart.download("%s.png" % filename)
-
-def usage():
-    print "pygchart.py -f <filename>"
-    sys.exit(2)
-        
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv,"hf:", [])
-
-    except getopt.GetoptError:
-        usage()
-
-    for opt, arg in opts:
-        if opt == "-h":
-            usage()
-        elif opt == "-f":
-            filename = arg
-        else:
-            usage()
-
-    (labels, values) = open_csv(filename)
-    plot(labels, values, filename)
+    chart.set_axis_labels(Axis.BOTTOM, dates)
+    chart.download("charts/%s.png" % fund)
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    for dirname, dirnames, filenames in os.walk('data/'):
+        for file in filenames:
+            print "Plotting file: data/%s.png" % file
+            plotter(file)
