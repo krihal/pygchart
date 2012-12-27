66bf6be3f6e003a6f2a9ce96f0e2fa5731994a8c
Kristofer Hallin <khallin@accedian.com>
2012-12-27 23:50:08 +0100

    Better colors

--- pygchart.py
+++ pygchart.py
@@ -24,16 +24,17 @@ def dataparser(filename):
 def plotter(fund):
     
     (dates, values) = dataparser("data/%s" % fund)
-
     left_axis = [int(min(values)), 0, int(max(values) + 1)]
-    left_axis[0] = 0
     
     chart = SimpleLineChart(600, 375, y_range=[min(values), max(values) + 1])
     chart.add_data(values)
-    chart.set_colours(['0000FF'])
-    chart.set_grid(0, 25, 5, 5)
+    chart.add_data([0] * 2)
+    chart.set_colours(['76A4FB'] * 5)
+    chart.add_fill_range('76A4FB', 0, 1)
+    chart.set_grid(0, 5, 1, 25)
     chart.set_axis_labels(Axis.LEFT, left_axis)
     chart.set_axis_labels(Axis.BOTTOM, dates)
+
     chart.download("charts/%s.png" % fund)
 
 if __name__ == '__main__':
